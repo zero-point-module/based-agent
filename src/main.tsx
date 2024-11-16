867598641f7041a4c26661ae76cd7f4a762a8a63
nvcho <ipmain07@gmail.com>
2024-11-16 04:33:33 -0300

    chore: setup tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles/main.css';
+
 import App from './App.tsx';
+
+import './styles/main.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
